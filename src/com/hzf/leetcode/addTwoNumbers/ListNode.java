3e89b045ba86d5e6f76755524916fbe2b9a17be2
zf.huang <hzf978865104@foxmail.com>
2018-11-29 10:49:23 +0800

    init 1、2、3

---
+++ src/com/hzf/leetcode/addTwoNumbers/ListNode.java
@@ -0,0 +1,15 @@
+package com.hzf.leetcode.addTwoNumbers;
+
+/**
+ * Created by zf.huang on 2018.11.28
+ */
+class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/com/hzf/leetcode/twoSum/Solution.java
@@ -0,0 +1,61 @@
+package com.hzf.leetcode.twoSum;
+
+import java.util.HashMap;
+
+/**
+ * Created by zf.huang on 2018.11.28.
+ * <p>
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        long startTime = System.currentTimeMillis();   //获取开始时间
+        int[] answer = new Solution().twoSum2(nums, target);
+        long endTime = System.currentTimeMillis(); //获取结束时间
+        System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
+        for (int num : answer) {
+            System.out.println(num);
+        }
+    }
+
+
+    private int[] twoSum1(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            for (int j = 0; j < nums.length; j++) {
+                if (temp == nums[j] && i != j) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
+    private int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> m = new HashMap<>();
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; ++i) {
+            if (m.containsKey(target - nums[i])) {
+                res[0] = i;
+                res[1] = m.get(target - nums[i]);
+                break;
+            }
+            m.put(nums[i], i);
+        }
+        return res;
+    }
+}
